7b2adddbef059c4351c6dfe35dd69c7a4b844bf3
Elkin Urango <kin3xc@hotmail.com>
2018-09-17 22:36:18 -0500

    Added connection wit REST API

--- src/providers/categorias/categorias.ts
+++ src/providers/categorias/categorias.ts
@@ -3,26 +3,26 @@ import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/toPromise';
 /*
-  Generated class for the ProvidersClientesProvider provider.
+  Generated class for the CategoriasProvider provider.
 
   See https://angular.io/docs/ts/latest/guide/dependency-injection.html
   for more info on providers and Angular DI.
 */
 @Injectable()
-export class ProvidersClientesProvider {
+export class CategoriasProvider {
   API_URL:string;
   constructor(public http: Http) {
     this.API_URL = "http://localhost:5434/api/v1";
   }
 
-  getClientes(categoria) {
-	  return this.http.get(this.API_URL+'/proveedor/categoria/'+categoria)
+  getCategorias() {
+	  return this.http.get(`${this.API_URL}/categoria`)
 	  .map(res => res.json())
 	  .toPromise();
 	}
 
-  getEventos(id) {
-    return this.http.get(`${this.API_URL}/producto/categoria/${id}`)
+  getEventos() {
+    return this.http.get(this.API_URL+'/producto/eventos')
     .map(res => res.json())
     .toPromise();
   }
