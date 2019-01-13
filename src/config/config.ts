90a9add803949bd418a32bb706d8ad3cbd64fc26
Elkin Urango <kin3xc@hotmail.com>
2019-01-13 13:27:55 -0500

    Added API for prod

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,6 @@ export interface IAppConfig {
     API_URL: string;
 }
 export const AppConfig: IAppConfig = {
-    API_URL: "http://34.211.102.109:5434/api/v1" //production
+    API_URL: "http://142.93.246.187/api/v1" //production
     // API_URL: 'http://localhost:5434/api/v1'   // development
 };
