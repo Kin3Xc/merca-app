175990c3992ebef1831eb75fa96f8df77d358bc9
Elkin Urango <kin3xc@hotmail.com>
2019-04-30 06:56:04 -0500

    Fixed performance to load app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,61 +1,37 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HttpModule } from '@angular/http';
+import { NgModule, ErrorHandler } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { IonicApp, IonicModule, IonicErrorHandler } from "ionic-angular";
+import { MyApp } from "./app.component";
+import { HttpModule } from "@angular/http";
 
-import { Geolocation } from '@ionic-native/geolocation';
+import { Geolocation } from "@ionic-native/geolocation";
 
 // import pages
-import { HomePage } from '../pages/home/home';
-import { MapaPage } from '../pages/mapa/mapa';
-import { CarritoPage } from '../pages/carrito/carrito';
-import { FavoritosPage } from '../pages/favoritos/favoritos';
+import { HomePage } from "../pages/home/home";
+import { CarritoPage } from "../pages/carrito/carrito";
 
-import { MasPage } from '../pages/mas/mas';
-import { LoginPage } from '../pages/login/login';
-import { TabsPage } from '../pages/tabs/tabs';
-import { StatusPage } from '../pages/status/status';
+import { MasPage } from "../pages/mas/mas";
+import { TabsPage } from "../pages/tabs/tabs";
+import { StatusPage } from "../pages/status/status";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
-
-import { ProvidersClientesProvider } from '../providers/providers-clientes/providers-clientes';
-import { ProvidersProductosProvider } from '../providers/providers-productos/providers-productos';
-import { CategoriasProvider } from '../providers/categorias/categorias';
-import { PedidoProvider } from '../providers/pedido/pedido';
+import { ProvidersClientesProvider } from "../providers/providers-clientes/providers-clientes";
+import { ProvidersProductosProvider } from "../providers/providers-productos/providers-productos";
+import { CategoriasProvider } from "../providers/categorias/categorias";
+import { PedidoProvider } from "../providers/pedido/pedido";
 
 /* import { Push } from '@ionic-native/push/ngx'; */
 
-
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    MapaPage,
-    CarritoPage,
-    FavoritosPage,
-    
-    MasPage,
-    LoginPage,
-    TabsPage,
-    StatusPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-    HttpModule
-  ],
+  declarations: [MyApp, HomePage, CarritoPage, MasPage, TabsPage, StatusPage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp), HttpModule],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    MapaPage,
     CarritoPage,
-    FavoritosPage,
-    
-    LoginPage,
     MasPage,
     TabsPage,
     StatusPage
@@ -64,11 +40,11 @@ import { PedidoProvider } from '../providers/pedido/pedido';
     StatusBar,
     SplashScreen,
     Geolocation,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
     ProvidersClientesProvider,
     ProvidersProductosProvider,
     CategoriasProvider,
-    PedidoProvider,
+    PedidoProvider
     /* Push */
   ]
 })
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,26 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { HomePage } from '../home/home';
-// import { MapaPage } from '../mapa/mapa';
-import { CarritoPage } from '../carrito/carrito';
-import { FavoritosPage } from '../favoritos/favoritos';
-
-// import { LoginPage } from '../login/login';
-import { MasPage } from '../mas/mas';
+import { HomePage } from "../home/home";
+import { CarritoPage } from "../carrito/carrito";
+import { MasPage } from "../mas/mas";
 
 @Component({
-  templateUrl: 'tabs.html'
+  templateUrl: "tabs.html"
 })
 export class TabsPage {
-
   tab1Root = HomePage;
   tab2Root = CarritoPage;
-  tab3Root = FavoritosPage;
-  
-  // tab5Root = LoginPage;
   tab6Root = MasPage;
 
-  constructor() {
-
-  }
+  constructor() {}
 }
