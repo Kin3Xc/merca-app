6b3b445b6d5930189d4842459173d6cbe60ec7f8
Elkin Urango <kin3xc@hotmail.com>
2019-07-07 19:55:47 -0500

    Added methhod for notifications

--- src/providers/socket.ts
+++ src/providers/socket.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@angular/core";
+/* import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
 
 import * as io from "socket.io-client";
@@ -16,6 +16,36 @@ export class SocketIO {
     this.socket.emit("pedido", message);
   }
 
+  public getPedido = () => {
+    console.log("on subscribe socket");
+    return Observable.create(observer => {
+      this.socket.on("pedido", pedido => {
+        observer.next(pedido);
+      });
+    });
+  };
+}
+ */
+
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+// import { AppConfig } from "../config";
+import * as io from "socket.io-client";
+
+@Injectable()
+export class SocketIO {
+  private url = "http://142.93.246.187";
+  private socket;
+
+  constructor() {
+    this.socket = io(this.url);
+  }
+
+  public sendPedido(pedido) {
+    console.log(pedido);
+    this.socket.emit("pedido", pedido);
+  }
   public getPedido = () => {
     console.log("on subscribe socket");
     return Observable.create(observer => {
