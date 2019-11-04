4a4114eadb4bd08eee47efd57ce0eed189687b4f
Elkin Urango <kin3xc@hotmail.com>
2019-11-03 21:24:05 -0500

    Added search and vueltas section

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
-import { ModalController, LoadingController } from 'ionic-angular';
+import { ModalController, LoadingController, AlertController, ToastController } from 'ionic-angular';
 import { ProvidersClientesProvider } from '../../providers/providers-clientes/providers-clientes';
 import { CategoriasProvider } from '../../providers/categorias/categorias';
+import { ProvidersProductosProvider } from '../../providers/providers-productos/providers-productos';
+import { FormControl } from '@angular/forms';
+import 'rxjs/add/operator/debounceTime';
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-home',
@@ -12,20 +16,150 @@ export class HomePage {
   public clientes:any = [];
   public loading:any;
   public eventos:any =[];
+  public productos:any =[];
   categorias: any = [];
   sliders: any = [];
   public itemSelected: string;
+  search = new FormControl('');
+  public abierto = true;
+  public ever = false;
 
   constructor(
     public modal: ModalController, 
     private clienteProvider: ProvidersClientesProvider,
     private loadingCtrl: LoadingController,
-    private _categorias: CategoriasProvider) {}
+    private _categorias: CategoriasProvider,
+    private _productos: ProvidersProductosProvider,
+    private Toast: ToastController,
+    private alertCtrl: AlertController) {
+
+      this.search
+      .valueChanges
+      .debounceTime(600)
+      .subscribe(value => this.getProducts(value));
+
+    }
   
   ionViewDidEnter() {
     this.categorias = [];
     this.getCategorias();
     this.getSliders();
+  }
+
+  getProducts(value) {
+    if (value && value.trim() != '') {
+      this.showLoading();
+      this._productos.search(value).then(res => {
+        if(res.length > 0){
+          res.forEach(producto => {
+            if(producto.proveedor && !producto.proveedor.ever && producto.proveedor.open && producto.proveedor.close){
+              const result = this.isNowBetweenTime(producto.proveedor.open, producto.proveedor.close); 
+              if(result){
+                producto.abierto = true;
+              } else {
+                producto.abierto = false;
+              }
+            } else {
+              if(producto.proveedor && producto.proveedor.ever){
+                producto.ever = true;
+              } else {
+                producto.ever = false;
+              }
+            }
+          });
+        } else {
+          this.toast('No se encontrarón resultados :(');
+        }
+        this.productos = res;
+        this.loading.dismiss();
+      }, err => {
+        this.loading.dismiss();
+      })
+    } else {
+      this.productos = [];
+    }
+  }
+
+  isNowBetweenTime(startTime, endTime){
+    // Creating moment objects for the current day at the given time
+    var startMom = moment(startTime, 'HH:mm');
+    var endMom   = moment(endTime, 'HH:mm');
+    
+    if ( startMom.isAfter(endMom) ){
+      endMom.add(1, 'd');
+    }
+    
+    return moment().isBetween(startMom, endMom);
+  }
+
+  prepareOrder(producto) {
+    this.validateLocalOrder(producto);
+  }
+
+  validateLocalOrder(producto){
+    let carrito = JSON.parse(localStorage.getItem('carritoPideYa'));
+    if(carrito){
+      if(carrito.comercio._id !== producto.proveedor._id){
+        const confirm = this.alertCtrl.create({
+          title: 'Información',
+          message: `Ya tienes productos de otro local en el carrito, solo puedes pedir de un solo sitio a la vez`,
+          buttons: [
+            {
+              text: 'Cancelar',
+              handler: () => {
+              }
+            },
+            {
+              text: 'Vaciar carrito y continuar',
+              handler: () => {
+                localStorage.removeItem('carritoPideYa');
+                this.addProducto(producto);
+              }
+            }
+          ]
+        });
+        confirm.present();
+      } else {
+        this.addProducto(producto);
+      }
+    } else {
+      this.addProducto(producto);
+    }
+  }
+
+  addProducto(producto){
+    let pushProduct = true;
+    producto.cantidad = 1;
+    let carrito = JSON.parse(localStorage.getItem('carritoPideYa'));
+    if(carrito && carrito.productos){
+      carrito.productos.forEach((item, index) => {
+        if(item._id === producto._id){
+          carrito.productos[index].cantidad += 1;
+          pushProduct = false;
+        }
+      });
+      if(pushProduct){
+        carrito.productos.push(producto);
+        localStorage.setItem('carritoPideYa', JSON.stringify(carrito));
+      } else {
+        localStorage.setItem('carritoPideYa', JSON.stringify(carrito));
+      }
+    } else {
+      localStorage.setItem('carritoPideYa', JSON.stringify({
+        comercio: producto.proveedor,
+        productos: [producto]
+      }));  
+    }
+    this.toast('Se agregó el producto a su pedido');
+  }
+
+  toast(message){
+    let toast = this.Toast.create({
+      message: message,
+      duration: 2000,
+      position: 'top'
+    });
+    toast.present();
   }
 
   getCategorias() {
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from "@angular/core";
 import { HomePage } from "../home/home";
 import { CarritoPage } from "../carrito/carrito";
 import { MasPage } from "../mas/mas";
+import { VueltasPage} from '../vueltas/vueltas';
 
 @Component({
   templateUrl: "tabs.html"
@@ -10,6 +11,7 @@ import { MasPage } from "../mas/mas";
 export class TabsPage {
   tab1Root = HomePage;
   tab2Root = CarritoPage;
+  tab3Root = VueltasPage;
   tab6Root = MasPage;
 
   constructor() {}
